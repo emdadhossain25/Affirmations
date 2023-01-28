233d2956ea2c728c798d70f54c75d4f4b1ca31f9
emdad <emdad@ergo-ventures.com>
2023-01-28 15:50:42 +0600

    added imageview for recyclerview

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -27,6 +28,7 @@ class ItemAdapter(
     // recyclerview reuses this viewholder to generate and fillup screen with views
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -40,8 +42,9 @@ class ItemAdapter(
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item=dataset[position]
-        holder.textView.text=context.resources.getString(item.stringResourceId)
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int = dataset.size
--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,3 +1,15 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+// as both constructor params are Int
+// for caution we use annotation for user info
+data class Affirmation(
+    //string resource annotation for strings.xml
+    @StringRes
+    val stringResourceId: Int,
+    // drawable resource annotation for drawable.xml
+    @DrawableRes
+    val imageResourceId: Int
+)
